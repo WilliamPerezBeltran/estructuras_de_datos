02919ac3505a12b3c1c3ed0af615cf35be301e6f
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2019-10-31 08:44:41 -0500

    busqueda lineal

--- 4_ordenamiento_por_insercion.py
+++ 4_ordenamiento_por_insercion.py
@@ -4,23 +4,24 @@ Ordenamiento por selección
 es un algoritmo que consiste en ordenar los elementos de manera acendente odescendente 
 
 fucniamiento 
--Recorremos cada elemento de la lista
--Cada ellemento de la lista se ordena si a suizquirerda tiene un elemento mayor que el actual
+-Recorremos cada elemento de la a
+-Cada ellemento de la a se ordena si a suizquirerda tiene un elemento mayor que el actual
 -Si es correcto el paso anteiror, se hace el intercambio de valores
 -El elemento se sigue recorriendo hacia la  izquierda hasta que tenga un elemento menor que el 
 
 
 """
-lista = [1,5,7,3,2,48,52,11,10,21,35,68,1,5,7,48,0]
-print(lista)
+import pdb
+a = [10,4,6,5,8,7,1,0]
+print(a)
+print('----')
 
-for i in range(1,len(lista)):
-	po = i
-	cu = a[i]
-	if a[i] < a[a-1]:
-		for x in range(po,-1,-1):
-			pivote = a[x-1]
-			a[x-1] = a[x]
-			a[x] = pivote
-	
-print(lista)
+for i in range(1,len(a)):
+	if a[i] < a[i-1]:
+		for x in range(i,-1,-1):
+			if  x>0 and a[x] < a[x-1]:
+				pivote = a[x]
+				a[x] = a[x-1]
+				a[x-1] = pivote
+	print(a)
+
